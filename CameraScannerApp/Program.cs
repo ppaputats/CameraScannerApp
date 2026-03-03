a583031572c54e1355ffdf027f64c3a953e483e4
Paul Paput <paul.paput@atslaser.fr>
2026-03-03 08:11:19 +0100

    MAJ avec 3 cams

--- CameraScannerApp/Program.cs
+++ CameraScannerApp/Program.cs
@@ -12,13 +12,12 @@ namespace CameraScannerApp
     class Program
     {
         // ID unique des cameras
-        public string[] Serial_list_cam = { "2678", "9872", "0981" };
+        public string[] Serial_list_cam = { "4110052366", "4110052371", "4110052372" };
         static void Main(string[] args)
         {
             try
             {
                 Program program = new Program();
-
                 string path_bridge = ConfigurationManager.AppSettings["Bridge"];
                 string path_photo = ConfigurationManager.AppSettings["Photo Folder"];
 
@@ -32,7 +31,7 @@ namespace CameraScannerApp
 
                 // On recherche la camera a appeler
                 int type_camera = program.numero_cam_reader(lignes[0]);
-
+                path_photo = Path.Combine(path_photo, lignes[0]);
                 program.prise_photo(type_camera, path_photo);
             }
             catch (Exception ex)
@@ -53,7 +52,7 @@ namespace CameraScannerApp
             // Conversion en int
             int resultat = int.Parse(valueStr);
 
-            Console.WriteLine(resultat); // Affiche 1
+            Console.WriteLine(resultat);
             return resultat;
         }
 
@@ -73,13 +72,13 @@ namespace CameraScannerApp
                 {
                     lignes[1] = "false";
                 }
+
                 // Sauvegarde des changements
                 File.WriteAllLines(path_bridge, lignes);
                 return;
-            }
+                }
             else
             {
-
             }
         }
 
@@ -89,14 +88,14 @@ namespace CameraScannerApp
             {
                 Program program = new Program();
                 int i = 0;
-                // 1. Initialisation
+
+                // Initialisation
                 IDSImaging.Peak.API.Library.Initialize();
                 var deviceManager = DeviceManager.Instance();
                 deviceManager.Update();
                 var devices = deviceManager.Devices();
 
-                // List all available devices.
-
+                // Liste des caméras reconnues :
                 Console.WriteLine("Devices available: ");
 
                 foreach (var DeviceDescriptor in devices)
@@ -105,30 +104,24 @@ namespace CameraScannerApp
                     string uniqueID = DeviceDescriptor.SerialNumber();
                     // Le nom complet du modèle
                     string modelName = DeviceDescriptor.ModelName();
-
                     Console.WriteLine($"MODELE : {modelName} | ID UNIQUE : {uniqueID}");
                 }
 
-                // 2. Ouverture de la caméra
-                // Récupération de l'ID cible
+                // Ouverture de la caméra et récupération de l'ID cible
                 string serialAttendu = Serial_list_cam[type_camera];
-
-                // --- Étape 2 : Chercher la caméra correspondante ---
-                // On filtre la liste des caméras branchées pour trouver CELLE qui a ce Serial exact
                 var deviceDescriptor = devices.FirstOrDefault(d => d.SerialNumber() == serialAttendu);
 
                 if (deviceDescriptor == null)
                 {
                     Console.WriteLine($"[ERREUR] La caméra attendue (SN: {serialAttendu}) n'est pas détectée sur le port USB.");
-
-                    // On informe l'UI principale que la mission a échoué
+                    // On informe l'UI que la mission a échoué
                     this.ack_write(false);
 
                     // ON S'ARRÊTE ICI : Le code suivant (ouverture et capture) ne sera jamais exécuté
                     return;
                 }
 
-                // --- Étape 3 : Ouverture uniquement si tout est OK ---
+                // Ouverture uniquement si tout est OK 
                 using var device = deviceDescriptor.OpenDevice(DeviceAccessType.Control);
                 Console.WriteLine($"[OK] Caméra {serialAttendu} identifiée et ouverte.");
                 Console.WriteLine($"Connecté à : {device.ModelName()}");
@@ -136,76 +129,91 @@ namespace CameraScannerApp
                 var remoteNodeMap = device.RemoteDevice().NodeMaps()[0];
                 using var dataStream = device.DataStreams()[0].OpenDataStream();
 
-                // 3. Allocation de la mémoire 
+                // Allocation de la mémoire 
                 var payloadSize = (uint)remoteNodeMap.FindNode<IntegerNode>("PayloadSize").Value();
 
-                // On demande le minimum vital exigé par ta caméra et on ajoute 1 marge de sécurité
+                // On demande le minimum vital exigé par la caméra et on ajoute 1 marge de sécurité
                 var minBuffers = dataStream.NumBuffersAnnouncedMinRequired();
                 for (int iii = 0; iii < minBuffers + 1; iii++)
                 {
                     var buffer = dataStream.AllocAndAnnounceBuffer(payloadSize, IntPtr.Zero);
                 }
-
                 int photoCount = 1;
 
-                // 5. Boucle de capture
+                // Boucle de capture
                 while (true)
                 {
-                        try
+                    try
+                    {
+                        // Nettoyage TOTAL avant la photo
+                        dataStream.Flush(DataStreamFlushMode.DiscardAll);
+                        foreach (var buf in dataStream.AnnouncedBuffers())
                         {
-                            // A. Nettoyage TOTAL avant la photo
-                            dataStream.Flush(DataStreamFlushMode.DiscardAll);
-                            foreach (var buf in dataStream.AnnouncedBuffers())
-                            {
-                                dataStream.QueueBuffer(buf); // On remet tout au propre
-                            }
-
-                            // B. On lance la capture
-                            dataStream.StartAcquisition();
-                            remoteNodeMap.FindNode<CommandNode>("AcquisitionStart").Execute();
-
-                            // C. On attrape la première image qui arrive
-                            var finishedBuffer = dataStream.WaitForFinishedBuffer(5000);
-
-                            // D. On coupe l'acquisition IMMÉDIATEMENT
-                            remoteNodeMap.FindNode<CommandNode>("AcquisitionStop").Execute();
-                            dataStream.StopAcquisition();
-
-                            // E. Conversion et Sauvegarde
-                            var iplImage = new IDSImaging.Peak.IPL.Image(
-                                (IDSImaging.Peak.IPL.PixelFormatName)finishedBuffer.PixelFormat(),
-                                finishedBuffer.BasePtr(), finishedBuffer.Size(),
-                                finishedBuffer.Width(), finishedBuffer.Height());
-
-                            var converter = new IDSImaging.Peak.IPL.ImageConverter();
-                            var convertedImage = converter.Convert(iplImage, IDSImaging.Peak.IPL.PixelFormatName.RGBa8);
-
-                            IDSImaging.Peak.IPL.ImageWriter.Write(path_photo, convertedImage);
-
-                            Console.WriteLine($"[OK] Photo {photoCount} enregistrée dans {path_photo}");
-                            
-                            // On écris sur le pont que la photo a bien été prise
-                            program.ack_write(true);
-                            photoCount++;
-                            break;
+                            dataStream.QueueBuffer(buf); // On remet tout au propre
                         }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine($"[ERREUR] : {ex.Message}");
+
+                        // On lance la capture
+                        dataStream.StartAcquisition();
+                        remoteNodeMap.FindNode<CommandNode>("AcquisitionStart").Execute();
+
+                        // On attrape la première image qui arrive
+                        var trashBuffer = dataStream.WaitForFinishedBuffer(5000);
+                        dataStream.QueueBuffer(trashBuffer); // On le remet dans la file
+
+                        // On récupère la deuxième image
+                        var finishedBuffer = dataStream.WaitForFinishedBuffer(5000);
+
+                        // On coupe l'acquisition 
+                        remoteNodeMap.FindNode<CommandNode>("AcquisitionStop").Execute();
+                        dataStream.StopAcquisition();
+
+                        // Conversion et Sauvegarde
+                        var iplImage = new IDSImaging.Peak.IPL.Image(
+                            (IDSImaging.Peak.IPL.PixelFormatName)finishedBuffer.PixelFormat(),
+                            finishedBuffer.BasePtr(), finishedBuffer.Size(),
+                            finishedBuffer.Width(), finishedBuffer.Height());
+
+                        var converter = new IDSImaging.Peak.IPL.ImageConverter();
+                        var convertedImage = converter.Convert(iplImage, IDSImaging.Peak.IPL.PixelFormatName.RGBa8);
+                        IDSImaging.Peak.IPL.ImageWriter.Write(path_photo, convertedImage);
+                        Console.WriteLine($"[OK] Photo {photoCount} enregistrée dans {path_photo}");
+
+                        // On écris sur le pont que la photo a bien été prise
+                        program.ack_write(true);
+                        photoCount++;
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"[ERREUR] : {ex.Message}");
                         // On écris sur le pont que la photo a bien été prise
                         program.ack_write(false);
-                        // Sécurité vitale en cas de plantage
+
+                        // Sécurités  en cas de plantage
                         try { remoteNodeMap.FindNode<CommandNode>("AcquisitionStop").Execute(); } catch { }
-                            try { dataStream.StopAcquisition(); } catch { }
-                        }
+
+                        try { dataStream.StopAcquisition(); } catch { }
+                    }
                 }
 
-                // 6. Rendu de la mémoire à Windows
-                foreach (var buf in dataStream.AnnouncedBuffers())
+                // Rendu de la mémoire à Windows
+                try
                 {
-                    dataStream.RevokeBuffer(buf);
+                    // On vide les files d'attente pour débloquer les buffers
+                    dataStream.Flush(DataStreamFlushMode.DiscardAll);
+
+                    foreach (var buf in dataStream.AnnouncedBuffers())
+                    {
+                        dataStream.RevokeBuffer(buf);
+                    }
+                    Console.WriteLine("Buffers libérés avec succès.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erreur lors de la libération des buffers : {ex.Message}");
                 }
             }
+
             catch (Exception ex)
             {
                 Console.WriteLine($"Erreur Fatale : {ex.Message}");
